1aad6768e8a801cf1b1fb3a56b08008e2e934ef2
Glen Jayson Dmello <glendmello04@gmail.com>
2025-08-10 11:13:02 +0530

    sentiment analysis using groqcloud api

--- nest_backend/src/mood/mood.module.ts
+++ nest_backend/src/mood/mood.module.ts
@@ -3,12 +3,14 @@ import { MoodService } from './mood.service';
 import { MoodResolver } from './mood.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoodEntry, MoodEntrySchema } from './schemas/mood.schema';
+import { SentimentModule } from 'src/sentiment/sentiment.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: MoodEntry.name, schema: MoodEntrySchema },
     ]),
+    SentimentModule
   ],
   providers: [MoodService, MoodResolver],
 })
