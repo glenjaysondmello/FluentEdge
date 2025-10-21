e3124554e3c3d3676219b24e5ac82db30609f3b3
Glen Jayson Dmello <glendmello04@gmail.com>
2025-10-21 09:12:43 +0530

    overiding http base class for adding timeout for all requests

--- flutter_frontend/lib/client/graphql_client.dart
+++ flutter_frontend/lib/client/graphql_client.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+/// A custom HTTP client that adds a timeout to all requests.
+class TimeoutClient extends http.BaseClient {
+  final http.Client _inner;
+  final Duration _timeout;
+
+  TimeoutClient({required Duration timeout})
+    : _inner = http.Client(),
+      _timeout = timeout;
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    // For every request, apply the timeout.
+    return _inner.send(request).timeout(_timeout);
+  }
+}
 
 ValueNotifier<GraphQLClient> getGraphQLClient() {
+  final httpClient = TimeoutClient(timeout: Duration(seconds: 200));
+
   final httpLink = HttpLink(
     "https://fluentedge.onrender.com/graphql",
     defaultHeaders: {'x-apollo-operation-name': 'UploadFile'},
+    httpClient: httpClient,
   );
 
   final authLink = AuthLink(
