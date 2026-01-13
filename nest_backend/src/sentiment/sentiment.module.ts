c54d38005ced1cf456aec857aa55ac8d4b1a4a29
Glen Jayson Dmello <glendmello04@gmail.com>
2026-01-13 19:56:39 +0530

    deleting unwanted modules

--- nest_backend/src/sentiment/sentiment.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SentimentService } from './sentiment.service';
-
-@Module({
-  providers: [SentimentService],
-  exports: [SentimentService]
-})
-export class SentimentModule {}
